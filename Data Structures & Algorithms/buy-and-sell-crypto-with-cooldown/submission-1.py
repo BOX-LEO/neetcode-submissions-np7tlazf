4990437f28c9da4101578711c5d88486576d1ae2
BOX-LEO <64097091+BOX-LEO@users.noreply.github.com>
2026-04-01 13:49:31 -0400

    Add: buy-and-sell-crypto-with-cooldown - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        hold = -prices[0]
+        sell = 0
+        rest = 0
+        for p in prices[1:]:
+            # print(hold,sell,rest)
+            p_hold = hold
+            p_sell = sell
+            p_rest = rest
+            hold = max(p_hold,p_rest-p)
+            sell = p+p_hold
+            rest = max(p_sell,p_rest)
+        return max(sell,rest)
+            
+
+
+
