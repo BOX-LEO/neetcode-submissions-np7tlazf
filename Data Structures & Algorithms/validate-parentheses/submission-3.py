59d6561ea04fdec8a226fe45cc21d68631bbfb20
BOX-LEO <64097091+BOX-LEO@users.noreply.github.com>
2026-06-16 17:33:52 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {'(':')','[':']','{':'}'}
+        for c in s:
+            if c in pairs:
+                stack.append(c)
+            else:
+                if not stack or pairs[stack[-1]]!=c:
+                    return False
+                else:stack.pop()
+        return not stack
